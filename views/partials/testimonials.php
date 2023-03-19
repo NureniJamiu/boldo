b1dd97ce895fc49ed0ac52656456d538902e25a0
NureniJamiu <nurenijamiu951@gmail.com>
2023-03-20 00:32:26 +0100

    add testimonials section

---
+++ views/partials/testimonials.php
@@ -0,0 +1,38 @@
+<div class="testimonial-top">
+    <h4>An enterprise template to ramp up your company website</h4>
+</div>
+
+<div class="testimonial-bottom">
+    <div>
+        <p>“Buyer buzz partner network disruptive non-disclosure agreement business”</p>
+        <div>
+            <div class="testimonial-id-top"></div>
+            <div class="testimonial-id-bottom">
+                <span>Albus Dumbledore</span>
+                <span>Manager @Howarts</span>
+            </div>
+        </div>
+    </div>
+
+    <div>
+        <p>“Learning curve infrastructure value proposition advisor strategy user experience hypotheses investor design business model canvas seed money monetization.”</p>
+        <div>
+            <div class="testimonial-id-top"></div>
+            <div class="testimonial-id-bottom">
+                <span>Albus Dumbledore</span>
+                <span>Manager @Howarts</span>
+            </div>
+        </div>
+    </div>
+
+    <div>
+        <p>“Release facebook responsive web design business model canvas seed money monetization.”</p>
+        <div>
+            <div class="testimonial-id-top"></div>
+            <div class="testimonial-id-bottom">
+                <span>Albus Dumbledore</span>
+                <span>Manager @Howarts</span>
+            </div>
+        </div>
+    </div>
+</div>
