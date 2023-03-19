e39e0ad4ae1df3d18d556251eabde812fcb4fb57
NureniJamiu <nurenijamiu951@gmail.com>
2023-03-20 00:31:53 +0100

    add link for fontawesome icons

--- views/partials/header.php
+++ views/partials/header.php
@@ -6,4 +6,5 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Boldo</title>
     <link rel="stylesheet" href="styles/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
