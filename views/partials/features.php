7e38a7bacbbc9699f2384c0351153b93aa5e7fa4
NureniJamiu <nurenijamiu951@gmail.com>
2023-03-20 00:31:08 +0100

    add feature section

---
+++ views/partials/features.php
@@ -0,0 +1,31 @@
+<div class="features-section-top">
+    <div class="features-section-left">
+        <img src="assets/feature1.png" alt="feature-image">
+    </div>
+    <div class="features-section-right">
+        <div>
+            <h5>We connect our customers with the best, and help them keep up and stay open.</h5>
+            <ul>
+                <li><i class="fa-solid fa-circle-check fa-lg" style="color: #0a2640"></i> We connect our customers with the best.</li>
+                <li><i class="fa-solid fa-circle-check fa-lg" style="color: #0a2640"></i> Advisor success customer launch party.</li>
+                <li><i class="fa-solid fa-circle-check fa-lg" style="color: #0a2640"></i> Business-to-consumer long tail.</li>
+            </ul>
+            <p>Start now</p>
+        </div>
+    </div>
+</div>
+<div class="features-section-bottom">
+    <div class="features-section-left">
+        <img src="assets/feature2.png" alt="feature-image">
+    </div>
+    <div class="features-section-right">
+        <div>
+            <h5>We connect our customers with the best, and help them keep up and stay open.</h5>
+            <ul>
+                <li><i class="fa-solid fa-feather"></i> We connect our customers with the best.</li>
+                <li><i class="fa-solid fa-eye"></i> Advisor success customer launch party.</li>
+                <li><i class="fa-solid fa-sun"></i> Business-to-consumer long tail.</li>
+            </ul>
+        </div>
+    </div>
+</div>
