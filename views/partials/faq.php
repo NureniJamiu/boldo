cdfe325afca5b95b6f3d3a97f18a95b99a4b765b
NureniJamiu <nurenijamiu951@gmail.com>
2023-03-20 00:30:56 +0100

    add faq section

---
+++ views/partials/faq.php
@@ -0,0 +1,10 @@
+<div>
+    <img src="assets/faq.png" alt="team">
+    <div>
+        <h6>We connect our customers with the best, and help them keep up and stay open.</h6>
+        <div>
+            <p>We connect our customers with the best? <i class="fa-solid fa-circle-chevron-down fa-lg"></i></p>
+            <p>Android research & development rockstar? <i class="fa-solid fa-circle-chevron-down fa-lg"></i></p>
+        </div>
+    </div>
+</div>
