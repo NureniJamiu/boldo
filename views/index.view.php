cae5feaf118b45a74bc7c3131a0476659ea482c9
NureniJamiu <nurenijamiu951@gmail.com>
2023-03-20 00:30:41 +0100

    add service, testimonial, feature & faq

--- views/index.view.php
+++ views/index.view.php
@@ -1,8 +1,20 @@
+<?php require('partials/header.php')?>
 <body>
-    <section>
-        <?php require('partials/header.php')?>
+    <section class="header-section">
         <?php require('partials/navbar.php')?>
         <?php require('partials/main.php')?>
+    </section>
+    <section class="services-section">
+        <?php require('partials/services.php')?>
+    </section>
+    <section class="features-section">
+        <?php require('partials/features.php')?>
+    </section>
+    <section class="testimonials-section">
+        <?php require('partials/testimonials.php')?>
+    </section>
+    <section class="faq-section">
+        <?php require('partials/faq.php')?>
     </section>
 </body>
 </html>
