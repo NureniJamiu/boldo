59e91fc68ba581ddc239caf37fe12b64096c5628
NureniJamiu <nurenijamiu951@gmail.com>
2023-03-20 00:27:33 +0100

    add data to loop through

--- index.php
+++ index.php
@@ -1,4 +1,21 @@
 <?php
 
+$services = [
+    [
+        "img" => "assets/service1.png",
+        "text" => "Efficient work activity",
+        "url" => "#"
+    ],
+    [
+        "img" => "assets/service2.png",
+        "text" => "Effective time management",
+        "url" => "#"
+    ],
+    [
+        "img" => "assets/service3.png",
+        "text" => "Multiplied productivity",
+        "url" => "#"
+    ],
+];
 
 require "views/index.view.php";
